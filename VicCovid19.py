b3b7bd109d11e83a0eb2c84954384f8927d118e1
Michael Fu <michaelfu1998@gmail.com>
2020-07-19 23:08:53 +1000

    Update VicCovid19.py

--- VicCovid19.py
+++ VicCovid19.py
@@ -14,35 +14,35 @@ def fetch_data_from_URL_query():
     r = requests.get(url)
     return r.json()
     
-    def create_excel_with_LGA_data():
-        r_json = fetch_data_from_URL_query()
-        data = r_json['features']
-        
-        # data of LGA in VIC    
-        integrated_data = []
-        
-        # Columns in csv
-        lga_name = []
-        population = []
-        area_SQRKM = []
-        
-        for dic in data:
-            one_data = {}
-            
-            one_data['LGA_name'] = dic['attributes']['LGA_NAME19']
-            lga_name.append(dic['attributes']['LGA_NAME19'])
-            
-            one_data['Population'] = dic['attributes']['Population']
-            population.append(dic['attributes']['Population'])
-            
-            one_data['Area(SQRKM)'] = dic['attributes']['AREASQKM19']
-            area_SQRKM.append(dic['attributes']['AREASQKM19'])
-            
-            integrated_data.append(one_data)
-        
-        df = pd.DataFrame({'LGA_name': lga_name, 'Population': population, 'Area(SQRKM)': area_SQRKM})
-        df.to_excel('covid_data_VIC.xlsx', index=False)
-        return
+def create_excel_with_LGA_data():
+    r_json = fetch_data_from_URL_query()
+    data = r_json['features']
+
+    # data of LGA in VIC    
+    integrated_data = []
+
+    # Columns in csv
+    lga_name = []
+    population = []
+    area_SQRKM = []
+
+    for dic in data:
+        one_data = {}
+
+        one_data['LGA_name'] = dic['attributes']['LGA_NAME19']
+        lga_name.append(dic['attributes']['LGA_NAME19'])
+
+        one_data['Population'] = dic['attributes']['Population']
+        population.append(dic['attributes']['Population'])
+
+        one_data['Area(SQRKM)'] = dic['attributes']['AREASQKM19']
+        area_SQRKM.append(dic['attributes']['AREASQKM19'])
+
+        integrated_data.append(one_data)
+
+    df = pd.DataFrame({'LGA_name': lga_name, 'Population': population, 'Area(SQRKM)': area_SQRKM})
+    df.to_excel('covid_data_VIC.xlsx', index=False)
+    return
         
     
 def append_daily_cases(date):
