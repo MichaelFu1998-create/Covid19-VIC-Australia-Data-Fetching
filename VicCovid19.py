d0e1e5940ce804b63604c8517c146cd0d20dd352
Michael Fu <michaelfu1998@gmail.com>
2020-07-19 23:03:17 +1000

    Update VicCovid19.py

--- VicCovid19.py
+++ VicCovid19.py
@@ -8,16 +8,14 @@ import requests
 import pandas as pd
 from datetime import datetime as dt
 
-class VicCovid19:
+  
+def fetch_data_from_URL_query():
+    url = 'https://services1.arcgis.com/vHnIGBHHqDR6y0CR/arcgis/rest/services/Victorian_LGA_Cases/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&outSR=173&f=json'
+    r = requests.get(url)
+    return r.json()
     
-    
-    def fetch_data_from_URL_query(self):
-        url = 'https://services1.arcgis.com/vHnIGBHHqDR6y0CR/arcgis/rest/services/Victorian_LGA_Cases/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&outSR=173&f=json'
-        r = requests.get(url)
-        return r.json()
-    
-    def create_excel_with_LGA_data(self):
-        r_json = self.fetch_data_from_URL_query()
+    def create_excel_with_LGA_data():
+        r_json = fetch_data_from_URL_query()
         data = r_json['features']
         
         # data of LGA in VIC    
@@ -47,41 +45,34 @@ class VicCovid19:
         return
         
     
-    def append_daily_cases(self, date):
-        """this method will append new accumulative data as a new column to excel"""
-        """the variable - date will be append to the column, just for recognizing the current date"""
-        
-        r_json = self.fetch_data_from_URL_query()
-        data = r_json['features']
-          
-        # Columns in excel
-        last_updated = []
-        cases = []
-    
-        for dic in data:
-            time = str(dic['attributes']['LastUpdated'])
-            if time.isnumeric():
-                time = time[:10]
-                time = int(time)
-                time = dt.fromtimestamp(time).strftime('%Y-%m-%d %I:%M:%S %p')                    
-                last_updated.append(time)
-            else:
-                last_updated.append('null')
-            cases.append(dic['attributes']['Cases'])
-        
-        with open('covid_data_VIC.xlsx', 'rb') as f:
-            df = pd.read_excel(f, index=False)
-        
-        df['Last Updated'+date] = last_updated
-        df['Cases '+date] = cases
-        
-        df.to_excel('covid_data_VIC.xlsx', index=False)
-        return
-    
-    
-v = VicCovid19() 
-v.create_excel_with_LGA_data()
-v.append_daily_cases('0718')
-v.append_daily_cases('0719')
+def append_daily_cases(date):
+    """this method will append new accumulative data as a new column to excel"""
+    """the variable - date will be append to the column, just for recognizing the current date"""
 
+    r_json = self.fetch_data_from_URL_query()
+    data = r_json['features']
 
+    # Columns in excel
+    last_updated = []
+    cases = []
+
+    for dic in data:
+        time = str(dic['attributes']['LastUpdated'])
+        if time.isnumeric():
+            time = time[:10]
+            time = int(time)
+            time = dt.fromtimestamp(time).strftime('%Y-%m-%d %I:%M:%S %p')                    
+            last_updated.append(time)
+        else:
+            last_updated.append('null')
+        cases.append(dic['attributes']['Cases'])
+
+    with open('covid_data_VIC.xlsx', 'rb') as f:
+        df = pd.read_excel(f, index=False)
+
+    df['Last Updated'+date] = last_updated
+    df['Cases '+date] = cases
+
+    df.to_excel('covid_data_VIC.xlsx', index=False)
+    return
+   
